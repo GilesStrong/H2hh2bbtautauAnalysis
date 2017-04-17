a53dcc1369137bc29ff153b8c571adf0c96f0eef
Giles Chatham Strong <giles.chatham.strong@cern.ch>
2017-04-17 16:00:40 +0100

    Testing gen branch

--- MainAnalysis/bin/skim_analyser.cc
+++ MainAnalysis/bin/skim_analyser.cc
@@ -55,7 +55,7 @@ bool getGenParticles(edm::Handle<reco::GenParticleCollection>* genParticles,
 	/*Point hbb and htautau to the Higgs*/
 	bool hBBFound = false, hTauTauFound = false;
 	int nHiggs = 0;
-	for (int i = 0; i < genParticles->GetEntriesFast(); ++i) {
+	for (int i = 0; i < genParticles->GetEntries(); ++i) {
 		const reco::GenParticle* p = (*genParticles)[i];
 		if (std::abs(p->pdgId) == 25) { //Particle is Higgs
 			if (p->numberOfDaughters() >= 2) { //Daughters exists
