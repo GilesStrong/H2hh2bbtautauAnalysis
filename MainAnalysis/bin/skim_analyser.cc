f1d5315c5d653e2eb62830270c0bffa91639e5a5
Giles Chatham Strong <giles.chatham.strong@cern.ch>
2017-04-20 15:13:57 +0100

    Testing gen branch

--- MainAnalysis/bin/skim_analyser.cc
+++ MainAnalysis/bin/skim_analyser.cc
@@ -732,8 +732,8 @@ int main(int argc, char* argv[])
 							event.getByLabel(edm::InputTag("prunedGenParticles"), genParticles);
 							int gen_hBB_key, gen_hTauTau_key;
 							if (getGenParticles(genParticles, &gen_hBB_key, &gen_hTauTau_key)) { //If both Higgs found
-								reco::GenParticle gen_hBB = (*genParticles)[gen_hBB_key];
-								reco::GenParticle gen_bjet0 = &(gen_hBB.daughter(0));
+								reco::GenParticle& gen_hBB = (*genParticles)[gen_hBB_key];
+								reco::GenParticle* gen_bjet0 = gen_hBB->daughter(0);
 								// gen_bjet1 = gen_hBB->daughter(1);
 								// gen_tau0 = gen_hTauTau->daughter(0);
 								// gen_tau1 = gen_hTauTau->daughter(1);
