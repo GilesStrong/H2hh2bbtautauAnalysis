9dd7f36d5511523b7b7615df8cd7385d30bb3932
Giles Chatham Strong <giles.chatham.strong@cern.ch>
2017-05-03 10:53:14 +0100

    swicth to CSV

--- MainAnalysis/bin/skim_analyser.cc
+++ MainAnalysis/bin/skim_analyser.cc
@@ -91,7 +91,7 @@ std::pair<int, int> getJets(edm::Handle<std::vector<pat::Jet>> selectedjets, std
 		pair.first = leading.first;
 		pair.second = subLeading.first;
 	}  else if (mode == "CSV") { //Two highest CSV, ordered by pT
-		std::pair<int, double,> leading = std::pair<int, double>(-1,-1.0);
+		std::pair<int, double> leading = std::pair<int, double>(-1,-1.0);
 		std::pair<int, double> subLeading = std::pair<int, double>(-1,-1.0);
 		for(size_t i = 0; i < selectedjets->size(); ++i) {
 			if (selectedjets->at(i).bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags") > leading.second) {
